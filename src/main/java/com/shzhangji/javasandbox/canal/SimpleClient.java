756768e4b68b3d25c0e1ec5787a4862ed07f02a2
Ji ZHANG <zhangji87@gmail.com>
2017-08-12 19:31:01 +0800

    refactor to functions

--- src/main/java/com/shzhangji/javasandbox/canal/SimpleClient.java
+++ src/main/java/com/shzhangji/javasandbox/canal/SimpleClient.java
@@ -16,7 +16,6 @@ import com.alibaba.otter.canal.protocol.Message;
 public class SimpleClient {
 
     public static void main(String[] args) throws Exception {
-
         CanalConnector connector = CanalConnectors.newSingleConnector(
                 new InetSocketAddress("127.0.0.1", 11111), "example", "", "");
 
@@ -27,38 +26,46 @@ public class SimpleClient {
         while (true) {
             Message message = connector.getWithoutAck(100);
             long batchId = message.getId();
-            int size = message.getEntries().size();
-            if (batchId == -1 || size == 0) {
+            if (batchId == -1 || message.getEntries().isEmpty()) {
                 System.out.println("sleep");
                 Thread.sleep(3000);
                 continue;
             }
+            printEntries(message.getEntries());
+            connector.ack(batchId);
+        }
+    }
 
-            for (Entry entry : message.getEntries()) {
-                if (entry.getEntryType() != EntryType.ROWDATA) {
-                    continue;
-                }
-                RowChange rowChange = RowChange.parseFrom(entry.getStoreValue());
-                for (RowData rowData : rowChange.getRowDatasList()) {
-                    switch (rowChange.getEventType()) {
-                    case INSERT:
-                    case UPDATE:
-                        System.out.print("UPSERT ");
-                        printColumns(rowData.getAfterColumnsList());
-                        break;
-
-                    case DELETE:
-                        System.out.print("DELETE ");
-                        printColumns(rowData.getBeforeColumnsList());
-                        break;
-
-                    default:
-                        break;
-                    }
-                }
+    private static void printEntries(List<Entry> entries) throws Exception {
+        for (Entry entry : entries) {
+            if (entry.getEntryType() != EntryType.ROWDATA) {
+                continue;
             }
 
-            connector.ack(batchId);
+            RowChange rowChange = RowChange.parseFrom(entry.getStoreValue());
+            for (RowData rowData : rowChange.getRowDatasList()) {
+                switch (rowChange.getEventType()) {
+                case INSERT:
+                case UPDATE:
+                    System.out.print("UPSERT ");
+                    printColumns(rowData.getAfterColumnsList());
+
+                    if ("retl_buffer".equals(entry.getHeader().getTableName())) {
+                        String tableName = rowData.getAfterColumns(1).getValue();
+                        String pkValue = rowData.getAfterColumns(2).getValue();
+                        System.out.println("SELECT * FROM " + tableName + " WHERE id = " + pkValue);
+                    }
+                    break;
+
+                case DELETE:
+                    System.out.print("DELETE ");
+                    printColumns(rowData.getBeforeColumnsList());
+                    break;
+
+                default:
+                    break;
+                }
+            }
         }
     }
 
