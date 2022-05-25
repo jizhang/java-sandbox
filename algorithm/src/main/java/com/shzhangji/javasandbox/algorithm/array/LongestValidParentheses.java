836d33a86f68061e5b22e61915ae3409418777ce
Ji ZHANG <zhangji87@gmail.com>
2022-05-25 14:33:45 +0800

    algorithm: longest valid parentheses

---
+++ algorithm/src/main/java/com/shzhangji/javasandbox/algorithm/array/LongestValidParentheses.java
@@ -0,0 +1,41 @@
+package com.shzhangji.javasandbox.algorithm.array;
+
+import java.util.ArrayDeque;
+
+public class LongestValidParentheses {
+  public static void main(String[] args) {
+    var obj = new LongestValidParentheses();
+    System.out.println(obj.longestValidParentheses("(()")); // 2
+    System.out.println(obj.longestValidParentheses(")()())")); // 4
+    System.out.println(obj.longestValidParentheses("(())")); // 4
+    System.out.println(obj.longestValidParentheses("())()")); // 2
+
+    System.out.println(obj.longestValidParentheses("")); // 0
+    System.out.println(obj.longestValidParentheses("()(()")); // 2
+    System.out.println(obj.longestValidParentheses("()(())")); // 6
+    System.out.println(obj.longestValidParentheses("(()())")); // 6
+
+    System.out.println(obj.longestValidParentheses("()(()(()")); // 2
+  }
+
+  public int longestValidParentheses(String s) {
+    var stack = new ArrayDeque<Integer>();
+    stack.push(-1);
+
+    int longest = 0;
+    for (int i = 0; i < s.length(); ++i) {
+      if (s.charAt(i) == '(') {
+        stack.push(i);
+      } else { // )
+        stack.pop();
+        if (stack.isEmpty()) {
+          stack.push(i);
+        } else {
+          longest = Math.max(longest, i - stack.peek());
+        }
+      }
+    }
+
+    return longest;
+  }
+}
