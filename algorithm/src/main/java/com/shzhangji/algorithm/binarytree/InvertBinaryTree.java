ea12d355bd5654c222f01e49c6b14aa1342ceefc
Ji ZHANG <zhangji87@gmail.com>
2022-08-08 11:01:28 +0800

    Invert tree

--- algorithm/src/main/java/com/shzhangji/algorithm/binarytree/InvertBinaryTree.java
+++ algorithm/src/main/java/com/shzhangji/algorithm/binarytree/InvertBinaryTree.java
@@ -21,6 +21,17 @@ public class InvertBinaryTree {
   }
 
   public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+      return root;
+    }
+
+    var t = root.left;
+    root.left = root.right;
+    root.right = t;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
     return root;
   }
 
