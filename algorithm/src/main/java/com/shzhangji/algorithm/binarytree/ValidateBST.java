06e7d21eff2c6272b1f554d42d1b02cd61cd8f81
Ji ZHANG <zhangji87@gmail.com>
2022-08-09 11:25:57 +0800

    Validate binary search tree (#6)
    * Validate BST with inorder traversal
    
    * Validate with min/max value
    
    * Validate with up left/right nodes

---
+++ algorithm/src/main/java/com/shzhangji/algorithm/binarytree/ValidateBST.java
@@ -0,0 +1,79 @@
+package com.shzhangji.algorithm.binarytree;
+
+// https://leetcode.com/problems/validate-binary-search-tree/
+public class ValidateBST {
+  public static void main(String[] args) {
+    var root = new TreeNode(2);
+    root.left = new TreeNode(1);
+    root.right = new TreeNode(3);
+    var obj = new ValidateBST();
+    System.out.println(obj.isValidBST(root)); // true
+
+    root = new TreeNode(5);
+    root.left = new TreeNode(1);
+    root.right = new TreeNode(4);
+    root.right.left = new TreeNode(3);
+    root.right.right = new TreeNode(6);
+    System.out.println(obj.isValidBST(root)); // false
+
+    root = new TreeNode(5);
+    root.left = new TreeNode(4);
+    root.right = new TreeNode(6);
+    root.right.left = new TreeNode(3);
+    root.right.right = new TreeNode(7);
+    System.out.println(obj.isValidBST(root)); // false
+  }
+
+  public boolean isValidBST(TreeNode root) {
+    // return validateInorder(root);
+    // return validateMinMax(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    return validateNode(root, null, null);
+  }
+
+  TreeNode prev;
+
+  boolean validateInorder(TreeNode root) {
+    if (root.left != null && !validateInorder(root.left)) {
+      return false;
+    }
+
+    if (prev != null && prev.val >= root.val) {
+      return false;
+    }
+    prev = root;
+
+    if (root.right != null && !validateInorder(root.right)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  boolean validateMinMax(TreeNode root, long min, long max) {
+    if (root == null) {
+      return true;
+    }
+
+    if (root.val <= min || root.val >= max) {
+      return false;
+    }
+
+    return validateMinMax(root.left, min, root.val) && validateMinMax(root.right, root.val, max);
+  }
+
+  boolean validateNode(TreeNode root, TreeNode upLeft, TreeNode upRight) {
+    if (root == null) {
+      return true;
+    }
+
+    if (upLeft != null && root.val <= upLeft.val) {
+      return false;
+    }
+
+    if (upRight != null && root.val >= upRight.val) {
+      return false;
+    }
+
+    return validateNode(root.left, upLeft, root) && validateNode(root.right, root, upRight);
+  }
+}
