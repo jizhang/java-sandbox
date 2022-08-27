101b59b82e8087cea3237f925592176ffa1537f1
Ji ZHANG <zhangji87@gmail.com>
2022-08-27 18:28:51 +0800

    Use 1D-array

--- algorithm/src/main/java/com/shzhangji/algorithm/dp/Triangle.java
+++ algorithm/src/main/java/com/shzhangji/algorithm/dp/Triangle.java
@@ -17,27 +17,23 @@ public class Triangle {
 
   public int minimumTotal(List<List<Integer>> triangle) {
     int rows = triangle.size();
-    var states = new int[rows][rows];
-    states[0][0] = triangle.get(0).get(0);
+    var states = new int[rows];
+    states[0] = triangle.get(0).get(0);
     for (int i = 1; i < rows; ++i) {
-      states[i][0] = states[i - 1][0] + triangle.get(i).get(0);
-    }
-
-    for (int i = 1; i < rows; ++i) {
-      for (int j = 1; j <= i; ++j) {
-        if (j == i) {
-          states[i][j] = states[i - 1][j - 1] + triangle.get(i).get(j);
+      for (int j = i; j >= 0; --j) { // Iterate backward
+        if (j == 0) {
+          states[j] += triangle.get(i).get(j);
+        } else if (j == i) {
+          states[j] = states[j - 1] + triangle.get(i).get(j);
         } else {
-          states[i][j] = Math.min(states[i - 1][j - 1], states[i - 1][j]) + triangle.get(i).get(j);
+          states[j] = Math.min(states[j - 1], states[j]) + triangle.get(i).get(j);
         }
       }
     }
 
     int result = Integer.MAX_VALUE;
     for (int j = 0; j < rows; ++j) {
-      if (states[rows - 1][j] < result) {
-        result = states[rows - 1][j];
-      }
+      result = Math.min(result, states[j]);
     }
     return result;
   }
