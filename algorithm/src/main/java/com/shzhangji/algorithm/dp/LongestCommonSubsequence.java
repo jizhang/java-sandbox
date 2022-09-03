db20a9f795a37910843ac29d0ba9942044446603
Ji ZHANG <zhangji87@gmail.com>
2022-09-03 19:20:45 +0800

    Unique paths (#13)
    * Unique paths
    
    * Top down
    
    * LCS top down

--- algorithm/src/main/java/com/shzhangji/algorithm/dp/LongestCommonSubsequence.java
+++ algorithm/src/main/java/com/shzhangji/algorithm/dp/LongestCommonSubsequence.java
@@ -1,5 +1,7 @@
 package com.shzhangji.algorithm.dp;
 
+import java.util.Arrays;
+
 // https://leetcode.com/problems/longest-common-subsequence/
 public class LongestCommonSubsequence {
   public static void main(String[] args) {
@@ -10,6 +12,25 @@ public class LongestCommonSubsequence {
   }
 
   public int longestCommonSubsequence(String text1, String text2) {
+    var memo = new int[text1.length()][text2.length()];
+    Arrays.stream(memo).forEach(arr -> Arrays.fill(arr, -1));
+    return topDown(text1, text2, 0, 0, memo);
+  }
+
+  int topDown(String text1, String text2, int i, int j, int[][] memo) {
+    if (i == text1.length() || j == text2.length()) return 0;
+    if (memo[i][j] != -1) return memo[i][j];
+    int result;
+    if (text1.charAt(i) == text2.charAt(j)) {
+      result = topDown(text1, text2, i + 1, j + 1, memo) + 1;
+    } else {
+      result = Math.max(topDown(text1, text2, i + 1, j, memo), topDown(text1, text2, i, j + 1, memo));
+    }
+    memo[i][j] = result;
+    return result;
+  }
+
+  int bottomUp(String text1, String text2) {
     var states = new int[text1.length() + 1][text2.length() + 1];
     for (int i = 1; i <= text1.length(); ++i) {
       for (int j = 1; j <= text2.length(); ++j) {
---
+++ algorithm/src/main/java/com/shzhangji/algorithm/dp/UniquePaths.java
@@ -0,0 +1,44 @@
+package com.shzhangji.algorithm.dp;
+
+// https://leetcode.com/problems/unique-paths/
+public class UniquePaths {
+  public static void main(String[] args) {
+    var obj = new UniquePaths();
+    System.out.println(obj.uniquePaths(3, 7)); // 28
+    System.out.println(obj.uniquePaths(3, 2)); // 3
+  }
+
+  public int uniquePaths(int m, int n) {
+    return topDown(m, n);
+  }
+
+  int bottomUp(int m, int n) {
+    var dp = new int[m][n];
+    for (int i = 0; i < m; ++i) {
+      dp[i][0] = 1;
+    }
+    for (int j = 0; j < n; ++j) {
+      dp[0][j] = 1;
+    }
+
+    for (int i = 1; i < m; ++i) {
+      for (int j = 1; j < n; ++j) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+
+    return dp[m - 1][n - 1];
+  }
+
+  int topDown(int m, int n) {
+    var memo = new int[m + 1][n + 1];
+    return topDown(m, n, memo);
+  }
+
+  int topDown(int m, int n, int[][] memo) {
+    if (m == 1 || n == 1) return 1;
+    if (memo[m][n] > 0) return memo[m][n];
+    memo[m][n] = topDown(m - 1, n, memo) + topDown(m, n - 1, memo);
+    return memo[m][n];
+  }
+}
