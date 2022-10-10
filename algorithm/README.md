4bbffeceaf1e1532505406134680316dee05149d
Ji ZHANG <zhangji87@gmail.com>
2022-10-10 19:35:46 +0800

    Fixed sized sorted array

--- algorithm/README.md
+++ algorithm/README.md
@@ -4,8 +4,8 @@
 
 ### Array
 
-- [ ] Implement an array that supports dynamic expansion.
-- [ ] Implement a fixed-sized sorted array, that supports add, delete and update.
+- [x] Implement an array that supports dynamic expansion.
+- [x] Implement a fixed-sized sorted array, that supports add, delete and update.
 - [x] [Merge sorted array](https://leetcode.com/problems/merge-sorted-array/)
 - [x] [Three sum](https://leetcode.com/problems/3sum/)
 - [ ] [Majority element](https://leetcode.com/problems/majority-element/)
---
+++ algorithm/src/main/java/com/shzhangji/algorithm/array/FixedSizedSortedArray.java
@@ -0,0 +1,81 @@
+package com.shzhangji.algorithm.array;
+
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FixedSizedSortedArray {
+  public static void main(String[] args) {
+    var arr = new FixedSizedSortedArray(10);
+    arr.add(1);
+    arr.add(2);
+    arr.add(3);
+    arr.add(5);
+    arr.add(6);
+    arr.add(7);
+    arr.add(0);
+    System.out.println(arr);
+    arr.delete(2);
+    System.out.println(arr);
+  }
+
+  private final int[] elements;
+  private int size;
+
+  public FixedSizedSortedArray(int maxSize) {
+    elements = new int[maxSize];
+    size = 0;
+  }
+
+  public void add(int element) {
+    if (size == elements.length) throw new IllegalStateException("Array is full.");
+    int index = findLastLte(element) + 1;
+    for (int i = size; i > index; --i) {
+      elements[i] = elements[i - 1];
+    }
+    elements[index] = element;
+    ++size;
+  }
+
+  public void delete(int element) {
+    int index = binarySearch(element);
+    if (index == -1) throw new NoSuchElementException();
+    for (int i = index + 1; i < size; ++i) {
+      elements[i - 1] = elements[i];
+    }
+    --size;
+  }
+
+  private int binarySearch(int target) {
+    int low = 0, high = size - 1;
+    while (low <= high) { // 1.
+      int mid = low + (high - low) / 2; // 2.
+      if (elements[mid] == target) return mid;
+      if (elements[mid] < target) low = mid + 1; // 3.
+      else high = mid - 1;
+    }
+    return -1;
+  }
+
+  private int findLastLte(int target) {
+    int low = 0, high = size - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (elements[mid] <= target) {
+        if (mid == size - 1 || elements[mid + 1] > target) return mid;
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return -1;
+  }
+
+  @Override
+  public String toString() {
+    var content = IntStream.range(0, size)
+      .mapToObj(i -> String.valueOf(elements[i]))
+      .collect(Collectors.joining(", "));
+    return "[" + content + "]";
+  }
+}
