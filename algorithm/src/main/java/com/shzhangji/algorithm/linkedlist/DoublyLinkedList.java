1d8b0d02b010613e217cbed615e7dfe60eb4a136
Ji ZHANG <zhangji87@gmail.com>
2022-10-13 10:38:49 +0800

    Doubly linked list

--- algorithm/src/main/java/com/shzhangji/algorithm/linkedlist/DoublyLinkedList.java
+++ algorithm/src/main/java/com/shzhangji/algorithm/linkedlist/DoublyLinkedList.java
@@ -9,25 +9,59 @@ public class DoublyLinkedList {
     head = new Node();
     tail = new Node();
     head.next = tail;
+    tail.prev = head;
   }
 
   // Append to the end.
   public boolean add(int element) {
+    var node = new Node(element, tail.prev, tail);
+    tail.prev.next = node;
+    tail.prev = node;
     return true;
   }
 
   public void add(int index, int element) {
     if (index < 0) throw new IndexOutOfBoundsException();
+
+    var current = head.next;
+    int i = 0;
+    while (current != tail) {
+      if (i == index) break;
+      current = current.next;
+      ++i;
+    }
+
+    if (i == index) {
+      var node = new Node(element, current.prev, current);
+      current.prev.next = node;
+      current.prev = node;
+    } else {
+      throw new IndexOutOfBoundsException();
+    }
   }
 
   // Remove first.
   public int remove() {
-    if (head.next == null) throw new NoSuchElementException();
+    if (head.next == tail) throw new NoSuchElementException();
+    head.next.next.prev = head;
+    head.next = head.next.next;
     return 0;
   }
 
   public int remove(int index) {
     if (index < 0) throw new IndexOutOfBoundsException();
+
+    var current = head.next;
+    int i = 0;
+    while (current != tail) {
+      if (i == index) {
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+        return current.value;
+      }
+      current = current.next;
+      ++i;
+    }
 
     throw new IndexOutOfBoundsException();
   }
@@ -40,9 +74,9 @@ public class DoublyLinkedList {
   public String toString() {
     var sb = new StringBuilder("[");
     var current = head.next;
-    while (current != null && current != tail) {
+    while (current != tail) {
       sb.append(current.value);
-      if (current.next != null) {
+      if (current.next != tail) {
         sb.append(", ");
       }
       current = current.next;
@@ -58,6 +92,12 @@ public class DoublyLinkedList {
 
     Node(int value) {
       this.value = value;
+    }
+
+    Node(int value, Node prev, Node next) {
+      this.value = value;
+      this.prev = prev;
+      this.next = next;
     }
   }
 }
--- algorithm/src/test/java/com/shzhangji/algorithm/linkedlist/DoublyLinkedListTest.java
+++ algorithm/src/test/java/com/shzhangji/algorithm/linkedlist/DoublyLinkedListTest.java
@@ -1,5 +1,6 @@
 package com.shzhangji.algorithm.linkedlist;
 
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +9,64 @@ class DoublyLinkedListTest {
   void testEmpty() {
     var list = new DoublyLinkedList();
     assertEquals("[]", list.toString());
+  }
+
+  @Test
+  void testAdd() {
+    var list = new DoublyLinkedList();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    assertEquals("[1, 2, 3]", list.toString());
+
+    list.add(0, 4);
+    assertEquals("[4, 1, 2, 3]", list.toString());
+    list.add(1, 5);
+    assertEquals("[4, 5, 1, 2, 3]", list.toString());
+    list.add(4, 6);
+    assertEquals("[4, 5, 1, 2, 6, 3]", list.toString());
+    list.add(6, 7);
+    assertEquals("[4, 5, 1, 2, 6, 3, 7]", list.toString());
+
+    assertThrows(IndexOutOfBoundsException.class, () -> {
+      list.add(-1, 0);
+    });
+
+    assertThrows(IndexOutOfBoundsException.class, () -> {
+      list.add(8, 0);
+    });
+  }
+
+  @Test
+  void testRemove() {
+    var list = new DoublyLinkedList();
+    list.add(1);
+    list.add(2);
+    list.remove();
+    assertEquals("[2]", list.toString());
+
+    list.remove();
+    assertThrows(NoSuchElementException.class, list::remove);
+
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    assertEquals(1, list.remove(0));
+    assertEquals(3, list.remove(1));
+    assertEquals(5, list.remove(2));
+    assertEquals("[2, 4]", list.toString());
+
+    list.remove();
+    assertThrows(IndexOutOfBoundsException.class, () -> {
+      list.remove(1);
+    });
+
+    assertEquals(4, list.remove(0));
+    assertEquals("[]", list.toString());
+    assertThrows(IndexOutOfBoundsException.class, () -> {
+      list.remove(0);
+    });
   }
 }
