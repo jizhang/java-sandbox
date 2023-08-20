d399f6ff10add77d3b1e4ec2240132123001cd37
Jerry <zhangji87@gmail.com>
2023-08-20 13:18:06 +0800

    Decorator pattern with Starbuzz Coffee

---
+++ pattern/src/main/java/com/shzhangji/pattern/decorator/Beverage.java
@@ -0,0 +1,11 @@
+package com.shzhangji.pattern.decorator;
+
+public abstract class Beverage {
+  String description = "Unknown beverage";
+
+  public String getDescription() {
+    return description;
+  }
+
+  public abstract double cost();
+}
---
+++ pattern/src/main/java/com/shzhangji/pattern/decorator/HouseBlend.java
@@ -0,0 +1,11 @@
+package com.shzhangji.pattern.decorator;
+
+public class HouseBlend extends Beverage {
+  public HouseBlend() {
+    description = "House Blend Coffee";
+  }
+
+  public double cost() {
+    return 0.89;
+  }
+}
