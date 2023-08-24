3654dd92b3f4baf4e64f7fc1bcf35c9bdfbf40de
Jerry <zhangji87@gmail.com>
2023-08-24 10:16:03 +0800

    Turkey adapter

---
+++ pattern/src/main/java/com/shzhangji/pattern/adapter/Duck.java
@@ -0,0 +1,6 @@
+package com.shzhangji.pattern.adapter;
+
+public interface Duck {
+  void quack();
+  void fly();
+}
---
+++ pattern/src/main/java/com/shzhangji/pattern/adapter/Turkey.java
@@ -0,0 +1,6 @@
+package com.shzhangji.pattern.adapter;
+
+public interface Turkey {
+  void gobble();
+  void fly();
+}
