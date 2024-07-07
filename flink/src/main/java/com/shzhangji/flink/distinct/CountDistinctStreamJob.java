b1cec88fdb9c3416080d1dac6bc5570db9843ac9
Jerry <zhangji87@gmail.com>
2024-07-07 10:38:23 +0800

    Keyed process function

--- flink/src/main/java/com/shzhangji/flink/distinct/CountDistinctStreamJob.java
+++ flink/src/main/java/com/shzhangji/flink/distinct/CountDistinctStreamJob.java
@@ -1,23 +1,30 @@
 package com.shzhangji.flink.distinct;
 
 import com.google.common.hash.Hashing;
-import java.time.Duration;
+import com.shzhangji.flink.proton.Utils;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.OpenContext;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.CheckpointingOptions;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.datagen.source.DataGeneratorSource;
 import org.apache.flink.connector.datagen.source.GeneratorFunction;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
-import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
-import org.apache.flink.streaming.api.windowing.triggers.ContinuousProcessingTimeTrigger;
 import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
 import org.apache.flink.util.Collector;
 
@@ -36,29 +43,33 @@ public class CountDistinctStreamJob {
     DataGeneratorSource<String> source = new DataGeneratorSource<>(
         generator, Long.MAX_VALUE, RateLimiterStrategy.perSecond(100), Types.STRING);
 
-    DataStream<String> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "data-gen");
-    stream.map(value -> Tuple2.of(value.substring(0, 1), value))
-        .returns(Types.TUPLE(Types.STRING, Types.STRING))
-        .keyBy(t -> t.f0)
-        .window(GlobalWindows.create())
-        .trigger(ContinuousProcessingTimeTrigger.of(Duration.ofSeconds(1)))
-        .aggregate(new DistinctAggregate(), new DistinctProcess())
+    DataStreamSource<String> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "data-gen");
+
+    KeySelector<String, String> key = value -> value.substring(0, 1);
+//    stream
+//        .keyBy(key)
+//        .window(GlobalWindows.create())
+//        .trigger(ContinuousProcessingTimeTrigger.of(Duration.ofSeconds(1)))
+//        .aggregate(new DistinctAggregate(), new DistinctProcessWindow())
+//        .print();
+
+    stream
+        .keyBy(key)
+        .process(new DistinctProcess())
         .print();
 
     env.execute();
-
-    // Use process function
   }
 
-  public static class DistinctAggregate implements AggregateFunction<Tuple2<String, String>, Set<String>, Long> {
+  public static class DistinctAggregate implements AggregateFunction<String, Set<String>, Long> {
     @Override
     public Set<String> createAccumulator() {
       return new HashSet<>();
     }
 
     @Override
-    public Set<String> add(Tuple2<String, String> value, Set<String> accumulator) {
-      accumulator.add(value.f1);
+    public Set<String> add(String value, Set<String> accumulator) {
+      accumulator.add(value);
       return accumulator;
     }
 
@@ -74,11 +85,43 @@ public class CountDistinctStreamJob {
     }
   }
 
-  public static class DistinctProcess extends ProcessWindowFunction<Long, Tuple2<String, Long>, String, GlobalWindow> {
+  public static class DistinctProcessWindow extends ProcessWindowFunction<Long, Tuple2<String, Long>, String, GlobalWindow> {
     @Override
     public void process(String key, Context context, Iterable<Long> elements, Collector<Tuple2<String, Long>> out) throws Exception {
       var count = elements.iterator().next();
       out.collect(Tuple2.of(key, count));
+    }
+  }
+
+  public static class DistinctProcess extends KeyedProcessFunction<String, String, Tuple2<String, Long>> {
+    private static final long INTERVAL = 5_000;
+
+    private transient MapState<String, Long> values;
+    private transient ValueState<Long> count;
+
+    @Override
+    public void open(OpenContext openContext) throws Exception {
+      values = getRuntimeContext().getMapState(new MapStateDescriptor<>("values", String.class, Long.class));
+      count = getRuntimeContext().getState(new ValueStateDescriptor<>("count", Long.class));
+    }
+
+    @Override
+    public void processElement(String value, Context ctx, Collector<Tuple2<String, Long>> out) throws Exception {
+      if (!values.contains(value)) {
+        values.put(value, 1L);
+        Long currentCount = count.value() == null ? 1L : count.value() + 1;
+        count.update(currentCount);
+      }
+
+      var timer = ctx.timerService();
+      var processingTime = timer.currentProcessingTime();
+      timer.registerProcessingTimeTimer((processingTime / INTERVAL + 1) * INTERVAL);
+    }
+
+    @Override
+    public void onTimer(long timestamp, OnTimerContext ctx, Collector<Tuple2<String, Long>> out) throws Exception {
+      // System.out.println(Utils.formatTimestampMillis(ctx.timerService().currentProcessingTime(), "yyyy-MM-dd HH:mm:ss"));
+      out.collect(Tuple2.of(ctx.getCurrentKey(), count.value()));
     }
   }
 }
